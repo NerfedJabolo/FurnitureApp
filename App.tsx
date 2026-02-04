6b1d7c84a226d75fc9257914530736bf4183dec9
NerfedJabolo <ezdlprofit1@gmail.com>
2026-02-04 03:50:14 +0200

    React native + nativewind setup and cleanup

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import './global.css';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>Hello</Text>
+      </SafeAreaView>
+    </>
+  );
+}
